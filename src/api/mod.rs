25e0fd2f09048ba4edb430f0438bbaf3289a14cd
WitcherTheWhite <1240749052@qq.com>
2025-12-31 09:48:14 +0800

    fix file name

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,7 +1,7 @@
 pub mod tee_api_arith_mpi;
 pub mod tee_api_cancel;
 pub mod tee_api_generic;
+pub mod tee_api_internal_client;
 pub mod tee_api_mm;
 pub mod tee_api_panic;
 pub mod tee_api_time;
-pub mod tee_internal_client;
